4172e4a709907d81a3ffb5d131123549366d9c52
Sroldano30 <sroldano30@gmail.com>
2025-10-01 12:48:32 -0500

    Se habilito el conexion otra vez

---
+++ conexion.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+
+namespace PortafolioDiseñadores
+{
+    public class Conexion
+    {
+        // Cambia la ruta si usas un .mdf en otra carpeta
+        private string cadena = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\USERS\SROLD\DESKTOP\PROYECTO_POO\PORTAFOLIO_DISENADORES\DBPORTAFOLIO.MDF;Integrated Security=True;Connect Timeout=30;";
+
+
+
+        public SqlConnection Abrir()
+        {
+            SqlConnection con = new SqlConnection(cadena);
+            con.Open();
+            return con;
+        }
+    }
+}
