3bd69e3c60caaf96b7fc47136600155484709524
Sroldano30 <sroldano30@gmail.com>
2025-10-12 16:34:21 -0500

    Decoracion en buscar diseñador

--- frmBuscarDiseñador.Designer.cs
+++ frmBuscarDiseñador.Designer.cs
@@ -31,10 +31,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.lblInstagram = new System.Windows.Forms.Label();
             this.lblUsuario = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.lblWhatsapp = new System.Windows.Forms.Label();
-            this.lblCorreo = new System.Windows.Forms.Label();
             this.lblBio = new System.Windows.Forms.Label();
+            this.lblCorreo = new System.Windows.Forms.Label();
+            this.lblWhatsapp = new System.Windows.Forms.Label();
             this.txtBuscar = new System.Windows.Forms.TextBox();
             this.btnBuscar = new System.Windows.Forms.Button();
             this.picProyectos = new System.Windows.Forms.PictureBox();
@@ -43,8 +42,9 @@
             this.btnAnterior = new System.Windows.Forms.Button();
             this.btnSiguiente = new System.Windows.Forms.Button();
             this.lblLikes = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.picProyectos)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -61,62 +61,44 @@
             // 
             this.lblInstagram.AutoSize = true;
             this.lblInstagram.Font = new System.Drawing.Font("Impact", 10F);
-            this.lblInstagram.Location = new System.Drawing.Point(16, 126);
+            this.lblInstagram.Location = new System.Drawing.Point(6, 123);
             this.lblInstagram.Name = "lblInstagram";
-            this.lblInstagram.Size = new System.Drawing.Size(66, 18);
+            this.lblInstagram.Size = new System.Drawing.Size(0, 18);
             this.lblInstagram.TabIndex = 2;
-            this.lblInstagram.Text = "Instagram";
             // 
             // lblUsuario
             // 
             this.lblUsuario.AutoSize = true;
             this.lblUsuario.Font = new System.Drawing.Font("Impact", 10F);
-            this.lblUsuario.Location = new System.Drawing.Point(16, 19);
+            this.lblUsuario.Location = new System.Drawing.Point(6, 16);
             this.lblUsuario.Name = "lblUsuario";
-            this.lblUsuario.Size = new System.Drawing.Size(53, 18);
+            this.lblUsuario.Size = new System.Drawing.Size(0, 18);
             this.lblUsuario.TabIndex = 3;
-            this.lblUsuario.Text = "Usuario";
             // 
-            // panel1
+            // lblBio
             // 
-            this.panel1.Controls.Add(this.lblBio);
-            this.panel1.Controls.Add(this.lblCorreo);
-            this.panel1.Controls.Add(this.lblWhatsapp);
-            this.panel1.Controls.Add(this.lblUsuario);
-            this.panel1.Controls.Add(this.lblInstagram);
-            this.panel1.Location = new System.Drawing.Point(39, 69);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(342, 162);
-            this.panel1.TabIndex = 4;
-            this.panel1.Tag = "";
+            this.lblBio.Font = new System.Drawing.Font("Impact", 10F);
+            this.lblBio.Location = new System.Drawing.Point(6, 49);
+            this.lblBio.Name = "lblBio";
+            this.lblBio.Size = new System.Drawing.Size(301, 58);
+            this.lblBio.TabIndex = 6;
+            // 
+            // lblCorreo
+            // 
+            this.lblCorreo.Font = new System.Drawing.Font("Impact", 10F);
+            this.lblCorreo.Location = new System.Drawing.Point(187, 123);
+            this.lblCorreo.Name = "lblCorreo";
+            this.lblCorreo.Size = new System.Drawing.Size(142, 18);
+            this.lblCorreo.TabIndex = 5;
             // 
             // lblWhatsapp
             // 
             this.lblWhatsapp.AutoSize = true;
             this.lblWhatsapp.Font = new System.Drawing.Font("Impact", 10F);
-            this.lblWhatsapp.Location = new System.Drawing.Point(104, 126);
+            this.lblWhatsapp.Location = new System.Drawing.Point(94, 123);
             this.lblWhatsapp.Name = "lblWhatsapp";
-            this.lblWhatsapp.Size = new System.Drawing.Size(65, 18);
+            this.lblWhatsapp.Size = new System.Drawing.Size(0, 18);
             this.lblWhatsapp.TabIndex = 4;
-            this.lblWhatsapp.Text = "Whatsapp";
-            // 
-            // lblCorreo
-            // 
-            this.lblCorreo.Font = new System.Drawing.Font("Impact", 10F);
-            this.lblCorreo.Location = new System.Drawing.Point(197, 126);
-            this.lblCorreo.Name = "lblCorreo";
-            this.lblCorreo.Size = new System.Drawing.Size(142, 18);
-            this.lblCorreo.TabIndex = 5;
-            this.lblCorreo.Text = "Correo";
-            // 
-            // lblBio
-            // 
-            this.lblBio.Font = new System.Drawing.Font("Impact", 10F);
-            this.lblBio.Location = new System.Drawing.Point(16, 52);
-            this.lblBio.Name = "lblBio";
-            this.lblBio.Size = new System.Drawing.Size(301, 58);
-            this.lblBio.TabIndex = 6;
-            this.lblBio.Text = "Biografia";
             // 
             // txtBuscar
             // 
@@ -151,9 +133,8 @@
             this.lblTitulo.Font = new System.Drawing.Font("Impact", 10F);
             this.lblTitulo.Location = new System.Drawing.Point(280, 237);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(40, 18);
+            this.lblTitulo.Size = new System.Drawing.Size(0, 18);
             this.lblTitulo.TabIndex = 8;
-            this.lblTitulo.Text = "Titulo";
             // 
             // lblDescripcion
             // 
@@ -162,7 +143,6 @@
             this.lblDescripcion.Name = "lblDescripcion";
             this.lblDescripcion.Size = new System.Drawing.Size(99, 94);
             this.lblDescripcion.TabIndex = 9;
-            this.lblDescripcion.Text = "Descripcion";
             // 
             // btnAnterior
             // 
@@ -192,9 +172,24 @@
             this.lblLikes.Font = new System.Drawing.Font("Impact", 10F);
             this.lblLikes.Location = new System.Drawing.Point(36, 446);
             this.lblLikes.Name = "lblLikes";
-            this.lblLikes.Size = new System.Drawing.Size(38, 18);
+            this.lblLikes.Size = new System.Drawing.Size(41, 18);
             this.lblLikes.TabIndex = 12;
-            this.lblLikes.Text = "Likes";
+            this.lblLikes.Text = "Likes:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblUsuario);
+            this.groupBox1.Controls.Add(this.lblBio);
+            this.groupBox1.Controls.Add(this.lblInstagram);
+            this.groupBox1.Controls.Add(this.lblWhatsapp);
+            this.groupBox1.Controls.Add(this.lblCorreo);
+            this.groupBox1.Font = new System.Drawing.Font("Impact", 8F);
+            this.groupBox1.Location = new System.Drawing.Point(36, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(351, 150);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Perfil";
             // 
             // frmBuscarDiseñador
             // 
@@ -202,6 +197,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSeaGreen;
             this.ClientSize = new System.Drawing.Size(425, 500);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.lblLikes);
             this.Controls.Add(this.btnSiguiente);
             this.Controls.Add(this.btnAnterior);
@@ -210,13 +206,12 @@
             this.Controls.Add(this.picProyectos);
             this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.txtBuscar);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Name = "frmBuscarDiseñador";
             this.Text = "frmBuscarDiseñador";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picProyectos)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -227,7 +222,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblInstagram;
         private System.Windows.Forms.Label lblUsuario;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lblBio;
         private System.Windows.Forms.Label lblCorreo;
         private System.Windows.Forms.Label lblWhatsapp;
@@ -239,5 +233,6 @@
         private System.Windows.Forms.Button btnAnterior;
         private System.Windows.Forms.Button btnSiguiente;
         private System.Windows.Forms.Label lblLikes;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
